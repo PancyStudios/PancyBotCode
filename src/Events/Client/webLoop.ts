b2999d6bf5802d7df0093ad51eb3cb8b4defa9f6
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2024-06-21 07:43:43 +0000

    WebLoop Disable

--- src/Events/Client/webLoop.ts
+++ src/Events/Client/webLoop.ts
@@ -4,18 +4,17 @@ import axios from 'axios'
 
 export default new Event('ready', async (client) => { 
     setTimeout(() => {
-        axios.post('https://califerbot.tk/api/info', {
-        apikey: process.env.PasswordApi,
-        info: {
-            guilds: client.guilds.cache.size,
-            users: client.users.cache.size,
-            channels: client.channels.cache.size}
-    }, {
-        maxRedirects: 100
-    }).then(x => {
-        logs.log(x.status as undefined)
-    }).finally(() => { logs.log('send')})
+        //axios.post('https://califerbot.tk/api/info', {
+        //apikey: process.env.PasswordApi,
+        //info: {
+        //    guilds: client.guilds.cache.size,
+        //    users: client.users.cache.size,
+        //    channels: client.channels.cache.size
+        //}
+        //}, {
+        //maxRedirects: 100
+        //}).then(x => {
+        //logs.log(x.status as undefined)
+        //}).finally(() => { logs.log('send')})
     }, 30 * 1000)
-
-
 })

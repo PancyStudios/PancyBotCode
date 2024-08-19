c8f557bb336b82cf51f29499b38cd7dcdd413f71
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2024-08-19 23:20:23 +0000

    Screenshot command

---
+++ src/commands/Guilds/subcommands/util/screenshot.ts
@@ -0,0 +1,34 @@
+import { firefox, Browser, Page } from "playwright";
+import { Command } from "../../../../Structure/CommandSlash";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+
+export default new Command({
+    name: "screenshot",
+    description: "Toma una captura de pantalla de una pagina web",
+    category: "util",
+    isDev: false,
+    botPermissions: ["EmbedLinks"],
+    options: [
+        {
+            name: "url",
+            description: "La url de la pagina web",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+
+    run: async ({ interaction }) => {
+        const url = interaction.options.getString("url", true);
+        const browser: Browser = await firefox.launch();
+        const context = await browser.newContext({
+            proxy: {
+                server: 'http://localhost:3128', // Puerto por defecto de Squid
+            }
+        });
+        const page: Page = await context.newPage();
+        await page.goto(url);
+        const buffer = await page.screenshot();
+        await interaction.reply({ embeds: [new EmbedBuilder().setImage("attachment://screenshot.png")], files: [{ attachment: buffer, name: "screenshot.png" }] });
+        await browser.close()
+    }
+})

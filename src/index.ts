ba4fbbc56f36eebff0eedec3545193398850f02e
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2024-08-19 22:32:16 +0000

    Other Fix

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,9 @@ import { ExtendedClient } from './Structure/Client'
 import { PancyBotUtils } from './Utils/Functions/BaseUtilsBot'
 import { ErrorHandler } from './Utils/Handlers/ErrorHandler/ErrorManageSystem'
 import { app } from './Utils/Handlers/Web'
-import { ClientV2 as Craiyon } from 'craiyon'
 export const errorHandler = new ErrorHandler()
 export const client = new ExtendedClient()
 export const utils = new PancyBotUtils()
-export const craiyon = new Craiyon()
 export var Server = app.listen(process.env.PORT, () => {
     process.env.PORT ? console.log(`Server running on port ${process.env.PORT}`) : console.log('Server running on port 3000')
 })

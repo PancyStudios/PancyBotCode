10cc1e945a149cfd2a88b15551ec34ec1efff545
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2024-08-19 22:38:32 +0000

    Testings

--- src/Events/Client/AutoUpdate.ts
+++ src/Events/Client/AutoUpdate.ts
@@ -1,9 +1,25 @@
 import { Event } from "../../Structure/Events";
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 export default new Event('ready', async (_) => {
     exec('git add .')
     exec('git commit -m "Local Changes"')
+    const installProcess = spawn(`yarn playwright install`)
+    installProcess.on('message', (message) => { 
+        console.log(message, 'PLAYWRIGHT INSTALL')
+    })
+    installProcess.on('error', (error) => {
+        console.log(error, 'PLAYWRIGHT INSTALL ERROR')
+    })
+    installProcess.on('close', (code) => {
+        console.log(code, 'PLAYWRIGHT INSTALL CLOSE')
+    })
+    installProcess.on('exit', (code) => {
+        console.log(code, 'PLAYWRIGHT INSTALL EXIT')
+    })
+    installProcess.on('disconnect', () => {
+        console.log('PLAYWRIGHT INSTALL DISCONNECT')
+    })
     setInterval(() =>{
         exec('git pull origin update --no-rebase', async (err, stdout, stderr) => {})
     }, 1000 * 60)

9352b5a09dc8a543c00c380bb6995d20218a0a0d
Turbiis <162517611+Turbiis@users.noreply.github.com>
2025-04-12 23:01:59 -0600

    Fix Errors

--- src/Utils/Functions/BaseUtilsBot.ts
+++ src/Utils/Functions/BaseUtilsBot.ts
@@ -130,41 +130,41 @@ async automoderator(client, mongoose, message, sanctionReason) {
         }
         return newArray;
     }
-    async ratelimitFilter(message: Message) {
-        if(usersWithCooldown.has(message.author.id)) {
-            let seeCooldown = await usersWithCooldown.get(message.author.id);
-            if(seeCooldown != new Date().getHours()) {
-                usersWithCooldown.delete(message.author.id);
-            }else return false;
-        }
-    
-        if(!cooldown.has(message.author.id)) {
-            cooldown.set(message.author.id, 1);
-        }
-    
-        let stop = await cooldown.get(message.author.id);
-    
-        if(stop >= 3) {
-            message.channel.send(`Debido a la inundación de comandos, has sido limitado (Es decir, no podrás usar comandos) durante ${60 - new Date().getMinutes()} minutos.`);
-            usersWithCooldown.set(message.author.id, parseInt(new Date().getHours().toString()));
-            return false;
-        }else{
-            
-            if(stop == 2) message.channel.send('Escribe los comandos de forma más lenta o serás limitado.').then(x => {
-                setTimeout(() => {
-                    x.delete();
-                }, 1500);
-            });
-    
-            cooldown.set(message.author.id, await cooldown.get(message.author.id) +1);
-    
-            setTimeout(async () => {
-                cooldown.set(message.author.id, await cooldown.get(message.author.id) -1);
-            }, 1000);
-    
-            return true;
-        }
-    }
+    // async ratelimitFilter(message: Message) {
+    //     if(usersWithCooldown.has(message.author.id)) {
+    //         let seeCooldown = await usersWithCooldown.get(message.author.id);
+    //         if(seeCooldown != new Date().getHours()) {
+    //             usersWithCooldown.delete(message.author.id);
+    //         }else return false;
+    //     }
+    //
+    //     if(!cooldown.has(message.author.id)) {
+    //         cooldown.set(message.author.id, 1);
+    //     }
+    //
+    //     let stop = await cooldown.get(message.author.id);
+    //
+    //     if(stop >= 3) {
+    //         message.channel.send(`Debido a la inundación de comandos, has sido limitado (Es decir, no podrás usar comandos) durante ${60 - new Date().getMinutes()} minutos.`);
+    //         usersWithCooldown.set(message.author.id, parseInt(new Date().getHours().toString()));
+    //         return false;
+    //     }else{
+    //
+    //         if(stop == 2) message.channel.send('Escribe los comandos de forma más lenta o serás limitado.').then(x => {
+    //             setTimeout(() => {
+    //                 x.delete();
+    //             }, 1500);
+    //         });
+    //
+    //         cooldown.set(message.author.id, await cooldown.get(message.author.id) +1);
+    //
+    //         setTimeout(async () => {
+    //             cooldown.set(message.author.id, await cooldown.get(message.author.id) -1);
+    //         }, 1000);
+    //
+    //         return true;
+    //     }
+    // }
 
     newResponse(response) {
         responses.set(response.authorId, response);

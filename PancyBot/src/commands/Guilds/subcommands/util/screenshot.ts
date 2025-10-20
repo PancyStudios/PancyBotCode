e26e294d6508dd8318cb011c5c63fddfc4b3388f
ImTurbis <73681786+ImTurbis@users.noreply.github.com>
2025-10-20 00:38:09 -0600

    ScreenShots page fix 3

--- PancyBot/src/commands/Guilds/subcommands/util/screenshot.ts
+++ PancyBot/src/commands/Guilds/subcommands/util/screenshot.ts
@@ -22,7 +22,7 @@ export default new Command({
         await interaction.deferReply();
 
         try {
-            const { data } = await axios.post<Buffer>('https://screenshot.pancy.miau.media/screenshot', {
+            const { data } = await axios.post<Buffer>('https://screenshot.pancy.miau.media/api/private/screenshot', {
                 url
             }, {
                 headers: {
